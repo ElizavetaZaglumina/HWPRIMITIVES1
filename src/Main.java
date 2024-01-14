70262e9759f570a29fd6fe9a43b9b33c021d2966
Elizaveta Zaglumina <zaglumina.elizaveta@gmail.com>
2024-01-14 16:53:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12530;
+        int mile = 20;
+        int bonus = price / mile;
+
+        System.out.println(bonus);
+    }
+}
